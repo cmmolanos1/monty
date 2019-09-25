e0c98e06acac72cc7bf91655ea10aabdaabb0c77
Carlos Molano <926@holbertonschool.com>
2019-09-25 11:11:44 -0500

    [Add] monty.h get_arg

--- monty.h
+++ monty.h
@@ -1,6 +1,10 @@
 #ifndef _MONTY_H_
 #define _MONTY_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -13,9 +17,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -28,8 +32,24 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+extern int global_number;
+
+int main(int argc, char *argv[]);
+int get_arg(char *argv[]);
+int exe(char *op, stack_t **stack, unsigned int line_number);
+void push(stack_t **head, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+int check_digit(char *tokens);
+void get_buffer(char *string, char **my_tokens);
+void free_stack_t(stack_t *head);
+void pint(stack_t **head, unsigned int line_number);
+void pop(stack_t **head, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
 
 #endif /* _MONTY_H_ */
