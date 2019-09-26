0c803223cb66be7fde8f057238574fb5475a8849
Carlos Molano <926@holbertonschool.com>
2019-09-26 08:41:38 -0500

    [Fix] pchar condition

--- pchar.c
+++ pchar.c
@@ -6,7 +6,7 @@
  */
 void pchar(stack_t **stack, unsigned int line_number)
 {
-	if (*stack == NULL)
+	if (stack == NULL || *stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		free(global.line);
@@ -21,6 +21,5 @@ void pchar(stack_t **stack, unsigned int line_number)
 		free_stack_t(global.newnode);
 		exit(EXIT_FAILURE);
 	}
-	putchar((*stack)->n);
-	putchar('\n');
+	printf("%c\n", (*stack)->n));
 }
